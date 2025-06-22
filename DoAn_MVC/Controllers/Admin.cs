200a48f9c353abb11152fedfbe59d9d0f6dfd0a0
kt9897224 <157088494+kt9897224@users.noreply.github.com>
2025-06-22 20:06:03 +0700

    Admin

---
+++ DoAn_MVC/Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn_MVC.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
